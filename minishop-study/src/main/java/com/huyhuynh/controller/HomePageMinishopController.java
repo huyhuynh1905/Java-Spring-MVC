20716a32312dd8ad727d6f00e106f28887df0687
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-29 20:47:48 +0700

    Spring MVC Resource file,classpath,http

--- minishop-study/src/main/java/com/huyhuynh/controller/HomePageMinishopController.java
+++ minishop-study/src/main/java/com/huyhuynh/controller/HomePageMinishopController.java
@@ -1,7 +1,13 @@
 package com.huyhuynh.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,7 +26,27 @@ public class HomePageMinishopController {
 	@ResponseBody
 	public String getHomeMessage() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("minishopcontainer.xml");
-		NhanVien nv = (NhanVien) context.getBean("nhanvien");
-		return "This is Home Page "+nv.toString();
+		//Resource resource = context.getResource("https://juliuskma.000webhostapp.com/getdatasinhvientojson.php");
+		
+		//Sử dụng ResourceLoaderAware để gán ngầm các context (cách khác)
+		/*DataLoader daLoader = new DataLoader();
+		daLoader.setResourceLoader(context);*/
+		DataLoader daLoader = (DataLoader) context.getBean("resourceDataLoader");
+		Resource resource = daLoader.getResource("https://juliuskma.000webhostapp.com/getdatasinhvientojson.php");
+		
+		try {
+			InputStream inputStream = resource.getInputStream();
+			BufferedReader bReader = new BufferedReader(new InputStreamReader(inputStream));
+			String line = "";
+			while((line = bReader.readLine())!=null) {
+				System.out.println(line+"\n");
+			}
+			bReader.close();
+			inputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "This is Home Page";
 	}
 }
